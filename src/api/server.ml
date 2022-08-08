764fed3aa1b1d8d40e48a48223ff4517981ed255
EliasGit2017 <azouaou.bendjaballah@etu.upmc.fr>
2022-08-08 11:16:04 +0200

    update `Server.load_testing_users` (default users now as global variable)

--- src/api/server.ml
+++ src/api/server.ml
@@ -2,35 +2,37 @@ open Data_types
 open Bcrypt
 open Db
 
+let user3 =
+  {
+    username = "tyler";
+    email = "tyler_durden@gmail.com";
+    password = "2ndRulerefRule1$!";
+    user_desc = "3rd user for testing only";
+    first_login_date = "2022-08-07 14:45:52.523274";
+  }
+
+let user2 =
+  {
+    username = "marla";
+    email = "marla1991@hotmail.fr";
+    password = "Rule1$!%";
+    user_desc = "Here for testing only";
+    first_login_date = "2022-08-07 14:45:52.523274";
+  }
+
+let user1 =
+  {
+    username = "james";
+    email = "james.dean@gmail.com";
+    password = "examPlePass1!";
+    user_desc = "test user 1";
+    first_login_date = "2022-08-07 14:45:52.523274";
+  }
+
+let default_users_list = [user1; user2; user3]
+
 let load_testing_users =
-  let user1 =
-    {
-      username = "james";
-      email = "james.dean@gmail.com";
-      password = "examPlePass1!";
-      user_desc = "test user 1";
-      first_login_date = "2022-08-07 14:45:52.523274";
-    }
-  in
-  let user2 =
-    {
-      username = "marla";
-      email = "marla1991@hotmail.fr";
-      password = "Rule1$!%";
-      user_desc = "Here for testing only";
-      first_login_date = "2022-08-07 14:45:52.523274";
-    }
-  in
-  let user3 =
-    {
-      username = "tyler";
-      email = "tyler_durden@gmail.com";
-      password = "2ndRulerefRule1$!";
-      user_desc = "3rd user for testing only";
-      first_login_date = "2022-08-07 14:45:52.523274";
-    }
-  in
-  let user_list = [ user1; user2; user3 ] in
+  (* let user_list = [ user1; user2; user3 ] in *) (* custom list of users *)
   let hashed_password_list =
     List.map
       (fun e ->
@@ -41,7 +43,7 @@ let load_testing_users =
           user_desc = e.user_desc;
           first_login_date = e.first_login_date;
         })
-      user_list
+      default_users_list
   in
   Lwt_list.map_s Db.add_user_to_db hashed_password_list
 
