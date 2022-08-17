4d2ccdba6f33f79b5058286a2bf6f2986446bbb1
EliasGit2017 <azouaou.bendjaballah@etu.upmc.fr>
2022-08-17 16:51:28 +0200

    added toml parsing utils :wolf:

--- src/backend_arch/config_verif.ml
+++ src/backend_arch/config_verif.ml
@@ -1,6 +1,8 @@
 open Otoml
+open Read_write_toml
 
 let () =
+  Printexc.record_backtrace true;
   let test =
     Otoml.Parser.from_file
       "/home/elias/OCP/ez_proofbox/src/backend_arch/job.toml"
@@ -9,5 +11,8 @@ let () =
   |> Otoml.Printer.to_channel ~indent_width:4 ~indent_subtables:true
        ~collapse_tables:false stdout;
   print_endline "\n ==> getting access to nested +/- values\n =>";
-  Otoml.find test Otoml.get_value ["owner"; "email"] |> Otoml.Printer.to_channel stdout
-  
+  print_endline "=> email : ";
+  Otoml.find test Otoml.get_value [ "owner"; "email" ]
+  |> Otoml.Printer.to_channel stdout;
+  print_endline "=> job_description table : ";
+  Utils.get_owner_bio test |> Otoml.Printer.to_string |> print_endline
--- src/backend_arch/utils.ml
+++ src/backend_arch/utils.ml
@@ -3,21 +3,47 @@ open Otoml
 (* Utils to get / set values and acces toml files simply &
    Wrappers over some functions *)
 
-let stringl_to_str s_l = 
-  List.fold_left ( ^ ) "" s_l
+let owner_username = [ "owner"; "username" ]
+let owner_email = [ "owner"; "email" ]
+let owner_bio = [ "owner"; "bio" ]
+let owner_password = [ "owner"; "password" ]
+let job_description_id = [ "job_description"; "job_id" ]
+let job_description_solver = [ "job_description"; "solver" ]
+let job_description_solver_version = [ "job_description"; "solver_version" ]
+let job_description_synopsis = [ "job_description"; "synopsis" ]
+let job_description_path_tof = [ "job_description"; "path_to_client_repo" ]
 
+(** Bad list to str *)
+let stringl_to_str s_l = List.fold_left ( ^ ) "" s_l
 
+(** change Otoml.TomlBoolean to bool (decaps/encaps) *)
 let get_value_wrap parsed_toml path_toval =
-  try Otoml.find parsed_toml path_toval with
-  | Otoml.Type_error e -> 
-  | Otoml.Key_error e -> 
+  try Otoml.find parsed_toml Otoml.get_value path_toval with
+  | Type_error e ->
+      print_endline
+      @@ Printf.sprintf "Otoml.Type_error from get_value_wrap : %s" e;
+      Otoml.TomlBoolean false
+  | Key_error e ->
+      print_endline
+      @@ Printf.sprintf "Otoml.Type_error from get_value_wrap : %s" e;
+      Otoml.TomlBoolean false
 
-let get_owner_username = ()
-let get_owner_bio = ()
-let get_owner_bio = ()
-let get_owner_password = ()
-let get_job_description_job_id = ()
-let get_job_description_solver = ()
-let get_job_description_solver_version = ()
-let get_job_description_job_synopsis = ()
-let get_job_description_path_tof = ()
+let get_owner_username parsed_toml = get_value_wrap parsed_toml owner_username
+let get_owner_bio parsed_toml = get_value_wrap parsed_toml owner_bio
+let get_owner_bio parsed_toml = get_value_wrap parsed_toml owner_bio
+let get_owner_password parsed_toml = get_value_wrap parsed_toml owner_password
+
+let get_job_description_job_id parsed_toml =
+  get_value_wrap parsed_toml job_description_id
+
+let get_job_description_solver parsed_toml =
+  get_value_wrap parsed_toml job_description_solver
+
+let get_job_description_solver_version parsed_toml =
+  get_value_wrap parsed_toml job_description_solver_version
+
+let get_job_description_job_synopsis parsed_toml =
+  get_value_wrap parsed_toml job_description_synopsis
+
+let get_job_description_path_tof parsed_toml =
+  get_value_wrap parsed_toml job_description_path_tof
